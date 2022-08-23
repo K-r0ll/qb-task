43016119a989cce150a31638ea8ce0dff8d1499b
K-r0ll <kmkarol7@gmail.com>
2022-08-23 21:38:03 +0200

    fix interface
    unecessary imports

--- src/main/java/org/example/services/ICurrencyService.java
+++ src/main/java/org/example/services/ICurrencyService.java
@@ -1,11 +1,10 @@
 package org.example.services;
 
-import org.apache.commons.csv.CSVRecord;
 import org.example.models.CurrencyInfo;
 
 public interface ICurrencyService {
 
-    public CurrencyInfo getSingleCurrencyFromCsv();
+     CurrencyInfo getSingleCurrencyFromCsv();
 
 
 }
